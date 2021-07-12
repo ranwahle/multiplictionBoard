cdd705f5d64659d3a9d15da056b0899c3f9376e4
yulias-av <yulias@avanan.com>
2021-07-12 20:07:07 +0300

    initial

--- server/index.js
+++ server/index.js
@@ -2,10 +2,25 @@ import express from 'express';
 
 const port = process.env.PORT || 3000;
 
+const progress = [];
+
 const app = express();
 app.use('/fontawesome', express.static('node_modules/@fortawesome/fontawesome-free/css'));
 app.use('/webfonts', express.static('node_modules/@fortawesome/fontawesome-free/webfonts'));
 app.use('/', express.static('client'))
+
+
+app.get('/api/progress', (req, res) => {
+    res.send(JSON.stringify(progress));
+    progress = [];
+})
+
+app.post('/api/progress', (req, res) => {
+    console.log(req.body);
+    const measureData = JSON.parse(req.body);
+    progress.push(measureData);
+    res.send(JSON.stringify(progress));
+})
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
