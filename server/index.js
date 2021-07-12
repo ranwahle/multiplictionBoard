1aa130d6f47b7133d05e3b2253a926f0b45d56c8
Ran Wahle <ran@ip-192-168-86-5.eu-west-1.compute.internal>
2021-07-12 20:20:43 +0300

    Use bodyParser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const port = process.env.PORT || 3000;
 
 const progress = [];
 
 const app = express();
+app.use(bodyParser.json());
 app.use('/fontawesome', express.static('node_modules/@fortawesome/fontawesome-free/css'));
 app.use('/webfonts', express.static('node_modules/@fortawesome/fontawesome-free/webfonts'));
 app.use('/', express.static('client'))
@@ -17,7 +19,7 @@ app.get('/api/progress', (req, res) => {
 
 app.post('/api/progress', (req, res) => {
     console.log(req.body);
-    const measureData = JSON.parse(req.body);
+    const measureData = req.body;
     progress.push(measureData);
     res.send(JSON.stringify(progress));
 })
